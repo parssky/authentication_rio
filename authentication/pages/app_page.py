4b4951fe6050816a772a08e3d5603c3a9a3b6138
Parsa <spacenavard1@gmail.com>
2025-05-30 10:56:02 +0330

    test

--- authentication/pages/app_page.py
+++ authentication/pages/app_page.py
@@ -18,7 +18,7 @@ def guard(event: rio.GuardEvent) -> str | None:
         event.session[data_models.AppUser]
 
     except KeyError:
-        # User is not logged in, redirect to the login page
+        # User is not logged in, redirect to the login page, TEST TEST
         return "/"
 
     # User is logged in, no redirection needed
